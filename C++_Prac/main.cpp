36e5a80eb81a4f85879421105e5985e77c50389d
NAKHOE KIM <nakhoekim@nakhoes-macbook-pro.local>
2015-11-28 17:28:23 +0900

    Initial Commit

---
+++ C++_Prac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++_Prac
+//
+//  Created by NAKHOE KIM on 11/28/15.
+//  Copyright © 2015 NAKHOE KIM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
